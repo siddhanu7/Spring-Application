7285561421238c016ea94dcdaa4809aa020ebe3d
siddhanu7 <siddh.anu7@gmail.com>
2021-05-14 16:31:48 -0400

    first commit

---
+++ demo/src/main/java/com/tts/demo/controller/BookIdMisMatchException.java
@@ -0,0 +1,20 @@
+package com.tts.demo.controller;
+
+public class BookIdMisMatchException extends Throwable {
+
+    public BookIdMisMatchException() {
+        super();
+    }
+
+    public BookIdMisMatchException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+
+    public BookIdMisMatchException(final String message) {
+        super(message);
+    }
+
+    public BookIdMisMatchException(final Throwable cause) {
+        super(cause);
+    }
+}
